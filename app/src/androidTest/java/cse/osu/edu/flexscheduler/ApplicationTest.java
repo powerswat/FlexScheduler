5263268574a33372799389a5ff26a5bb725bc5a9
powerswat <powerswat1@gmail.com>
2015-10-08 10:57:25 -0400

    Initial commit

---
+++ app/src/androidTest/java/cse/osu/edu/flexscheduler/ApplicationTest.java
@@ -0,0 +1,13 @@
+package cse.osu.edu.flexscheduler;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
