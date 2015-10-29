4a63e2e09d89990259c7fe6b7c5c4f3fbeb2a2ad
powerswat <powerswat1@gmail.com>
2015-10-29 11:19:50 -0400

    [Practice] Tried ListView
    Not represented

--- app/src/main/java/cse/osu/edu/flexscheduler/SchedListActivity.java
+++ app/src/main/java/cse/osu/edu/flexscheduler/SchedListActivity.java
@@ -1,15 +1,25 @@
 package cse.osu.edu.flexscheduler;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class SchedListActivity extends AppCompatActivity {
     private final static String  TAG = "SchedListAcitivity";
+    private ArrayList<NoticeData> notice_data = new ArrayList<NoticeData>();
+    Context mContext = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +27,13 @@ public class SchedListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sched_list);
 
         Log.d(TAG, "onCreate()");
+
+        notice_data.add(new NoticeData("공지사항내용1", "2015-03-22"));
+        notice_data.add(new NoticeData("공지사항내용2", "2015-03-25"));
+        notice_data.add(new NoticeData("공지사항내용3", "2015-03-28"));
+
+        ListView notice_list = (ListView) findViewById(R.id.listView);
+        NoticeListAdapter adapter = new NoticeListAdapter(mContext,0,notice_data);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -44,4 +61,27 @@ public class SchedListActivity extends AppCompatActivity {
     }
 
 
+    private class NoticeListAdapter extends ArrayAdapter<NoticeData> {
+
+        private ArrayList<NoticeData> mNoticeData;
+
+        public NoticeListAdapter(Context context, int resource, ArrayList<NoticeData> notice_data){
+            super(context, resource, notice_data);
+            mNoticeData = notice_data;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent){
+            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(LAYOUT_INFLATER_SERVICE);
+            View row_view = inflater.inflate(R.layout.list_notice, null, true);
+
+            TextView txtTitle = (TextView) row_view.findViewById(R.id.title);
+            TextView txtDate = (TextView) row_view.findViewById(R.id.date);
+
+            txtTitle.setText(mNoticeData.get(position).getTitle());
+            txtDate.setText(mNoticeData.get(position).getData());
+
+            return row_view;
+        }
+    }
 }
