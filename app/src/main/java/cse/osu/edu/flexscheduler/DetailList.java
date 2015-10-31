9b6107158f269495bd9732afd0e80e5b74441b76
jyun977 <jyun977@gmail.com>
2015-10-31 12:16:49 -0400

    Update Detail List

--- app/src/main/java/cse/osu/edu/flexscheduler/DetailList.java
+++ app/src/main/java/cse/osu/edu/flexscheduler/DetailList.java
@@ -1,7 +1,10 @@
 package cse.osu.edu.flexscheduler;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -11,7 +14,18 @@ public class DetailList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_list);
+
+      /*  FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new DetailListFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }*/
     }
+
 
 
     @Override
