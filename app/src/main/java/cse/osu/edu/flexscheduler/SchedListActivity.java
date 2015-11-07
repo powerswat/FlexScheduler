22902715566a752aa99d12d9803f3a1fc179970f
powerswat <powerswat1@gmail.com>
2015-11-07 16:42:48 -0500

    [Java] Replaced all Crimes with Scheds

--- app/src/main/java/cse/osu/edu/flexscheduler/SchedListActivity.java
+++ app/src/main/java/cse/osu/edu/flexscheduler/SchedListActivity.java
@@ -1,47 +1,40 @@
 package cse.osu.edu.flexscheduler;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
+import android.content.Intent;
+import android.support.v4.app.Fragment;
 
-public class SchedListActivity extends AppCompatActivity {
-    private final static String  TAG = "SchedListAcitivity";
+public class SchedListActivity extends SingleFragmentActivity
+        implements SchedListFragment.Callbacks, SchedFragment.Callbacks {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sched_list);
-
-        Log.d(TAG, "onCreate()");
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    protected Fragment createFragment() {
+        return new SchedListFragment();
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(TAG, "onResume()");
+    protected int getLayoutResId() {
+        return R.layout.activity_masterdetail;
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(TAG, "onPause()");
+    public void onSchedSelected(Sched sched) {
+        if (findViewById(R.id.detail_fragment_container) == null) {
+            Intent intent = SchedPagerActivity.newIntent(this, sched.getId());
+            startActivity(intent);
+        } else {
+            Fragment newDetail = SchedFragment.newInstance(sched.getId());
+
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.detail_fragment_container, newDetail)
+                    .commit();
+        }
     }
 
-
+    @Override
+    public void onSchedUpdated(Sched sched) {
+        SchedListFragment listFragment = (SchedListFragment)
+                getSupportFragmentManager()
+                        .findFragmentById(R.id.fragment_container);
+        listFragment.updateUI();
+    }
 }
